e8d3d837ea3369f8b83d383b47abe02f714a1df9
briheet <briheetyadav@gmail.com>
2026-01-01 17:04:38 +0530

    upd: read, write over sockets works
    Accept new conns, read, write over sockets works fine. Now need to add
    this up in a class with methods designed well. Was reading a system
    design paper on how to hide impl with api to make it clearer. will try
    to add that here.

--- src/main.cc
+++ src/main.cc
@@ -1,82 +1,141 @@
-#include <cstdint>
-#include <cstdio>
+#include <cstddef>
 #include <cstdlib>
+#include <cstring>
 #include <err.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/_types/_ssize_t.h>
 #include <sys/event.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
-class Kqueue {
-
-  int kq;
-  struct kevent event;
-  struct kevent triggeredEvent;
-
-public:
-  Kqueue(void) {
-    // Create kqueue
-    this->kq = kqueue();
-    if (kq == -1) {
-      err(EXIT_FAILURE, "kqueue failure");
-    }
-  }
-
-  void set_event(uintptr_t ident, int16_t filter, uint16_t flags,
-                 uint32_t fflags, intptr_t data, void *udata) {
-
-    EV_SET(&event, ident, filter, flags, fflags, data, udata);
-  }
-
-  int call_kevent(int nchanges, int nevents, const struct timespec *timeout) {
-
-    return kevent(kq, &event, nchanges, &triggeredEvent, nevents, timeout);
-  }
-
-  const struct kevent &get_triggered_event() { return triggeredEvent; }
-
-  ~Kqueue() { close(kq); }
-};
+// Plan: Create a socket, give it a address and port, listen for connections,
+// accept client and send and recieve data
+constexpr int MAX_EVENTS = 1024;
 
 int main(void) {
+  std::cout << "Hi" << "\n";
 
-  Kqueue kq;
+  int tcp_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (tcp_socket_fd == -1) {
+    err(EXIT_FAILURE, "tcp socket init error");
+  }
 
-  // Set event
-  kq.set_event(1, EVFILT_TIMER, EV_ADD | EV_ENABLE, 0, 500, NULL);
+  struct sockaddr_in tcp_socket_addr;
+  std::memset(&tcp_socket_addr, 0, sizeof(tcp_socket_addr));
 
-  int count = 5;
+  tcp_socket_addr.sin_len = sizeof(tcp_socket_addr); // BSD/Macos stuff
+  tcp_socket_addr.sin_family = AF_INET;
+  tcp_socket_addr.sin_port = htons(6379);
+  tcp_socket_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  int yes = 1;
+  setsockopt(tcp_socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+
+  // Bind
+  int ret =
+      bind(tcp_socket_fd, reinterpret_cast<struct sockaddr *>(&tcp_socket_addr),
+           sizeof(tcp_socket_addr));
+  if (ret == -1) {
+    err(EXIT_FAILURE, "bind issue\n");
+  }
+
+  ret = listen(tcp_socket_fd, 50);
+  if (ret == -1) {
+    err(EXIT_FAILURE, "listen error\n");
+  }
+
+  int flags = fcntl(tcp_socket_fd, F_GETFL, 0);
+  fcntl(tcp_socket_fd, F_SETFL, flags | O_NONBLOCK);
+
+  // Create a kqueue to watch for events
+  int kq = kqueue();
+  if (kq == -1) {
+    err(EXIT_FAILURE, "kqueue open failure\n");
+  }
+
+  struct kevent tcp_events{};
+  EV_SET(&tcp_events, tcp_socket_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0,
+         nullptr);
+
+  // Attach event in kqueue
+  ret = kevent(kq, &tcp_events, 1, nullptr, 0, nullptr);
+  if (ret == -1) {
+    err(EXIT_FAILURE, "unable to attach kevent in kqueue\n");
+  }
+
+  struct kevent events[MAX_EVENTS];
 
   for (;;) {
 
-    // Sleep untill something happens
-    int ret = kq.call_kevent(1, 1, NULL);
-    if (ret == -1) {
-      err(EXIT_FAILURE, "kevent wait");
-    } else if (ret > 0) {
-      const auto &ev = kq.get_triggered_event();
-      if (ev.flags & EV_ERROR) {
-        errx(EXIT_FAILURE, "Event error: %s", strerror(ev.data));
-      }
-
-      pid_t pid = fork();
-
-      if (pid < 0) {
-        err(EXIT_FAILURE, "fork()");
-      } else if (pid == 0) {
-        if (execlp("date", "date", (char *)0) < 0) {
-          err(EXIT_FAILURE, "execlp()");
-        }
-      }
+    int nev = kevent(kq, nullptr, 0, events, MAX_EVENTS, nullptr);
+    if (nev == -1) {
+      err(EXIT_FAILURE, "kevent_wait\n");
     }
 
-    count = count - 1;
+    for (int i = 0; i < nev; i++) {
 
-    if (count == 0)
-      break;
+      int event_fd = static_cast<int>(events[i].ident);
+      if (event_fd == tcp_socket_fd) {
+
+        // New client conn
+        for (;;) {
+          struct sockaddr client_addr{};
+          socklen_t addr_len = sizeof(client_addr);
+
+          int client_fd = accept(
+              tcp_socket_fd, reinterpret_cast<struct sockaddr *>(&client_addr),
+              &addr_len);
+
+          if (client_fd == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+              break; // accept queue drained
+            err(EXIT_FAILURE, "accept issue\n");
+          }
+
+          int flags = fcntl(client_fd, F_GETFL, 0);
+          if (flags == -1)
+            err(EXIT_FAILURE, "fcntl(F_GETFL)");
+          if (fcntl(client_fd, F_SETFL, flags | O_NONBLOCK) == -1)
+            err(EXIT_FAILURE, "fcntl(F_SETFL)");
+
+          struct kevent cev{};
+          EV_SET(&cev, client_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0,
+                 nullptr);
+
+          kevent(kq, &cev, 1, nullptr, 0, nullptr);
+        }
+      } else if (events[i].filter == EVFILT_READ) {
+        // Client read
+
+        char buf[4096];
+        ssize_t size = recv(event_fd, buf, sizeof(buf), 0);
+
+        // Debug
+        std::cout.write(buf, size);
+        std::cout << std::endl;
+
+        if (size == 0) {
+          close(event_fd);
+          continue;
+        }
+
+        if (size < 0) {
+
+          if (errno != EAGAIN && errno != EWOULDBLOCK)
+            close(event_fd);
+          continue;
+        }
+
+        char reply[] = "gotchya message\n";
+        send(event_fd, reply, sizeof(reply) - 1, 0);
+      }
+    }
   }
+
+  (void)close(tcp_socket_fd);
+  (void)close(kq);
 
   return EXIT_SUCCESS;
 }
